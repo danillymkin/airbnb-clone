1a18b9a0d42aaed7b7a0dd439d2d7aeefaf08e89
Danil Lyamkin <danilwebdev@gmail.com>
2023-12-28 15:11:17 +0300

    feat: add nunito font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ['@nuxtjs/google-fonts'],
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+    },
+  },
+  googleFonts: {
+    families: {
+      Nunito: true,
     },
   },
 })
