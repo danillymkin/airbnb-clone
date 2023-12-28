bf03291357726c6a70553559ec9887a41370f028
Danil Lyamkin <danilwebdev@gmail.com>
2023-12-28 18:18:06 +0300

    feat: start work on header

---
+++ components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="fixed w-full bg-white z-10 shadow-sm">
+    <div class="py-4 border-b">
+      <Container>
+        <div class="flex flex-row items-center justify-between gap-3 md:gap-0">
+          <Logo />
+        </div>
+      </Container>
+    </div>
+  </header>
+</template>
---
+++ components/Logo.vue
@@ -0,0 +1,3 @@
+<template>
+  <img src="/img/logo.png" alt="AirBnb" class="hidden md:block cursor-pointer w-[100px]">
+</template>
